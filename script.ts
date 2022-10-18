03c6bb7bb6ac4e07d7690fdef64736226caa8366
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-18 18:45:22 +0530

    feat: add query to create a new post

--- script.ts
+++ script.ts
@@ -21,6 +21,25 @@ async function main() {
    */
   // const users = await prisma.user.findMany();
   // console.log(users);
+  /**
+   * ? query to create a new post
+   * ! By default, Prisma only returns scalar fields
+   * ! in the result objects of a query.
+   */
+  const user = await prisma.user.create({
+    data: {
+      name: "UserName",
+      email: "username@gmail.com",
+      posts: {
+        create: {
+          title: "Hello World!",
+          content: "Lorem Ipsum",
+          published: true,
+        },
+      },
+    },
+  });
+  console.log(user);
 }
 
 main()
